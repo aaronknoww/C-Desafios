a63899ebdc6b7f9897bd7f8d781274bb433a4f98
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2023-08-13 22:57:50 +0000

    Coin Change in unique way is done and pass in GFG

--- coinChange.cpp
+++ coinChange.cpp
@@ -1,103 +1,35 @@
-//FALTA DEPURAR.
-//CREA LAS COMBINACIONES DE MONEDAS PERO, TARDA EN LOS CASOS DE GFG
-
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
+
+
+/// TERMINADO Y PASO LOS 1121 CASOS DE GFG
+
 // } Driver Code Ends
 class Solution {
   public:
-
     long long int count(int coins[], int N, int sum)
     {
-        
-        set<int> ordCoins(coins, coins+N);// To create a set and order every coin and remove duplicates
-        set<int>::iterator itCoin = ordCoins.begin();
-        unordered_map<int, set<int>> tbl;
-        unordered_map<int, set<int>>::iterator itTbl;
-    
+        vector<vector<long long>> dpWays(N + 1, vector<long long>(sum + 1, 0));
+        int itCoin = 0; // iterate thru every coin
 
-        int counter = 0;
-        if(N == 0 || sum < *ordCoins.begin())
-            return 0;
+        dpWays[0][0] = 1;
 
-        set<int> auxCode;
-        int db =  *ordCoins.begin();
-        
-        for(int s = *ordCoins.begin();s<=sum; s++)// to find every combination for sum 0 to sum n
+        for (int row = 1; row <= N; row++)// from the dpWays matrix 
         {
-            
-            if(ordCoins.find(s)!= ordCoins.end())
-                auxCode.insert(s);               
-                    
-            while(*itCoin < s)// to do every rest of coins sum - coin[0], sum - coin[1]...
+            dpWays[row][0] = 1;
+            for (int col = 1; col <= sum; col++)
             {
-                if((s - *itCoin) < *itCoin )
-                {
-                    itCoin++;
-                    continue;
-                }
-                itTbl = tbl.find(s - *itCoin);
-                if(itTbl != tbl.end())
-                {
-                    
-                    for(set<int>::iterator it = itTbl->second.begin(); it!=itTbl->second.end(); it++)
-                    {
-                        auxCode.insert(mergeCoin(*itCoin, *it));
-                        counter++;                        
-                    }
-                        
-                }
-                itCoin++;
-            }          
-            if(!auxCode.empty())
-            {
-                tbl.insert({s,auxCode});
-                auxCode.clear();
+                dpWays[row][col] = (col - coins[itCoin] >= 0) ? dpWays[row][col - coins[itCoin]] + dpWays[row - 1][col] : dpWays[row - 1][col];
             }
-            itCoin =  ordCoins.begin();
-
+            itCoin++;
         }
 
-        return counter;          
-        
+        return dpWays[N][sum];
+
         // code here.
     }
-
-private:
-    int mergeCoin(int coin, long long int codigo)
-    {
-        multiset<int> setCoin;
-        long long int result = 0;
-        long long int mul = 1;
-    
-
-        while(codigo > 0)
-        {
-            setCoin.insert(codigo%10);
-            codigo -= codigo%10; 
-            codigo=codigo/10;
-        }
-        setCoin.insert(coin);
-        for(multiset<int>::reverse_iterator ite = setCoin.rbegin(); ite != setCoin.rend(); ite++)
-        {
-            result += *ite * mul;
-            mul *= 10;
-        }
-        return result;  
-    
-    }
 };
-//{2,5,3,6}
-/*
-1
-7 3
-2 4 6
-
-1
-4 10
-2 5 3 6
-*/
 
 //{ Driver Code Starts.
 int main() {
